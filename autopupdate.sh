bc1b72a8a5687aa55826b84686fec5b985f0f873
Darren Fürst <77537563+Darren-Fuerst@users.noreply.github.com>
2022-03-14 08:22:20 +0100

    Update autopupdate.sh

--- autopupdate.sh
+++ autopupdate.sh
@@ -5,4 +5,7 @@ git pull https://github.com/Darren-Fuerst/Teststellen_Ausdrucke_GUI.git
 pip install pysimplegui
 pip install pandas
 
+# needed to write out to Excel files
+pip install openpyxl
+
 python update_popup.py
